08b05e32dd531819cb1e4beda91668ba327cdd32
Murad <m.iusupov>
2022-11-02 12:05:51 +0300

    integration Jetpack Navigation

--- app/src/main/java/com/example/playingnumbers/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/playingnumbers/presentation/ChooseLevelFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.playingnumbers.R
 import com.example.playingnumbers.databinding.FragmentChooseLevelBinding
 import com.example.playingnumbers.domain.entity.Level
@@ -47,23 +48,15 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(
+                level
+            )
+        )
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    companion object {
-
-        const val NAME = "ChooseLevelFragment"
-
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
     }
 }
--- app/src/main/java/com/example/playingnumbers/presentation/GameViewModelFactory.kt
+++ app/src/main/java/com/example/playingnumbers/presentation/GameViewModelFactory.kt
@@ -5,12 +5,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.playingnumbers.domain.entity.Level
 
+
 class GameViewModelFactory(
     private val level: Level,
     private val application: Application
-) : ViewModelProvider.Factory {
+): ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(GameViewModel::class.java)) {
             return GameViewModel(application, level) as T
         }
